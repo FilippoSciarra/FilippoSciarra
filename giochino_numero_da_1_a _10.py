8842c347e1a820eda13106dc48a5685f651f1ef1
Filippo Sciarra <114309412+FilippoSciarra@users.noreply.github.com>
2022-09-24 20:40:17 +0200

    Add files via upload

---
+++ giochino_numero_da_1_a _10.py
@@ -0,0 +1,26 @@
+import random
+
+n = random.randint(1, 10)
+print('Sto pensando ad un numero tra 1 e 10...')
+
+running = True
+while running:
+    numero_indovinato = input('Prova ad indovinare')
+    indovino = int(numero_indovinato)
+
+    if indovino == n:
+        print('Bravo, come hai fatto ad indovinare...')
+        
+        running = False
+
+    elif 0 < indovino < n:
+        print('No, mi spiace, troppo piccolo :(')
+
+    elif indovino < 1:
+        print('Ho detto da 1 a 10, non meno di 1! >:(')
+
+    elif indovino > 10:
+        print('Ho detto da 1 a 10, non più di 10! >:(')
+
+    elif n < indovino <= 10:
+        print('No, mi spiace, troppo grande :(')
